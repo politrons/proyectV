db6313c41c7232e95eb8053fc35f9bd46e5b8c04
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-08-28 11:48:17 +0100

    css on discography page

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Author Jérémie Grienenberger &  Pablo Perez Garcia 
 
 # Proyect V
+
+Project to continue learning on Scala and playFramework 
+
+To run the service, just need to run the Play2Run and then automatically the Service will open main page
+
+** Music search on itunes  http://localhost:9000/search
--- app/controllers/BaseController.scala
+++ app/controllers/BaseController.scala
@@ -4,15 +4,22 @@ import http.HttpClient._
 import model.Discography
 import play.api.mvc._
 
+import scala.util.parsing.json.JSONArray
+
 class BaseController extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def search(artist:String) = Action {
-    get(s"itunes.apple.com/search?term=${artist.replace(" ", "+")}", "https")
-    Ok(views.html.search(Discography.create(lastResponse.get)))
+  def search() = Action { implicit request =>
+    Ok(views.html.discography(Discography.create(new JSONArray(List()))))
+  }
+
+  def discography = Action { implicit request =>
+    val artist: Option[String] = request.getQueryString("artist")
+    get(s"itunes.apple.com/search?term=${artist.get.replace(" ", "+")}", "https")
+    Ok(views.html.discography(Discography.create(lastResponse.get)))
   }
 
 }
