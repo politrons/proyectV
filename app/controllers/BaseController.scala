d3672c76527f4f966d6507bb919cf6cb6e388a59
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-09-17 13:36:24 +0100

    Add cache system

--- app/controllers/BaseController.scala
+++ app/controllers/BaseController.scala
@@ -1,17 +1,22 @@
 package controllers
 
+import javax.inject.Inject
+
 import http.HttpClient._
-import model.app.AppleStore
-import model.movie.AppleTv
-import model.music.Discography
+import implicits.Utils.optionUtils
+import model.app.{AppleStore, Application}
+import model.movie.{AppleTv, Movie}
+import model.music.{Album, Discography}
+import play.api.cache._
 import play.api.mvc._
 import views.html
 
+import scala.concurrent.duration._
 import scala.util.parsing.json.JSONArray
 
-class BaseController extends Controller {
+class BaseController @Inject()(cache: CacheApi) extends Controller {
 
-   var country="us"
+  var country = "us"
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -19,7 +24,7 @@ class BaseController extends Controller {
 
   def selectCountry = Action { implicit request =>
     val newCountry: Option[String] = request.getQueryString("country")
-    country=newCountry.get
+    country = newCountry.get
     Ok(views.html.index("Your new application is ready."))
   }
 
@@ -29,11 +34,19 @@ class BaseController extends Controller {
     val artist: Option[String] = request.getQueryString("artist")
     if (artist.isEmpty) {
       Ok(html.discography(Discography.albums(new JSONArray(List()))))
-    }else {
-      get(s"$appleAPI${artist.get.replace(" ", "+")}", "https")
-      val albums = Discography.albums(lastResponse.get)
-      get(s"$appleAPI${artist.get.replace(" ", "+").concat(s"&country=$country&entity=musicVideo")}", "https")
-      Discography.attachVideos(lastResponse.get, albums)
+    } else {
+      val cacheKey=artist.asString+"-"+country
+      var albums: List[Album] = List()
+      val albumsCached = cache.get(cacheKey)
+      if (albumsCached.isEmpty) {
+        get(s"$appleAPI${artist.get.replace(" ", "+")}", "https")
+        albums = Discography.albums(lastResponse.get)
+        get(s"$appleAPI${artist.get.replace(" ", "+").concat(s"&country=$country&entity=musicVideo")}", "https")
+        Discography.attachVideos(lastResponse.get, albums)
+        cache.set(cacheKey, albums, 5.minutes)
+      } else {
+        albums = albumsCached.get
+      }
       Ok(html.discography(albums))
     }
   }
@@ -43,9 +56,18 @@ class BaseController extends Controller {
     if (app.isEmpty) {
       Ok(html.application(AppleStore.applications(new JSONArray(List()))))
     } else {
-      get(s"$appleAPI${app.get.replace(" ", "+").concat(s"&country=$country&entity=software")}", "https")
-      val apps = AppleStore.applications(lastResponse.get)
+      val cacheKey=app.asString+"-"+country
+      var apps: List[Application] = List()
+      val appsCached = cache.get(cacheKey)
+      if (appsCached.isEmpty) {
+        get(s"$appleAPI${app.get.replace(" ", "+").concat(s"&country=$country&entity=software")}", "https")
+        apps = AppleStore.applications(lastResponse.get)
+        cache.set(cacheKey, apps, 5.minutes)
+      } else {
+        apps = appsCached.get
+      }
       Ok(html.application(apps))
+
     }
   }
 
@@ -54,8 +76,16 @@ class BaseController extends Controller {
     if (movie.isEmpty) {
       Ok(html.movie(AppleTv.movies(new JSONArray(List()))))
     } else {
-      get(s"$appleAPI${movie.get.replace(" ", "+").concat(s"&country=$country&entity=movie")}", "https")
-      val movies = AppleTv.movies(lastResponse.get)
+      val cacheKey=movie.asString+"-"+country
+      var movies: List[Movie] = List()
+      val moviesCached = cache.get(cacheKey)
+      if (moviesCached.isEmpty) {
+        get(s"$appleAPI${movie.get.replace(" ", "+").concat(s"&country=$country&entity=movie")}", "https")
+        movies = AppleTv.movies(lastResponse.get)
+        cache.set(cacheKey, movies, 5.minutes)
+      } else {
+        movies = moviesCached.get
+      }
       Ok(html.movie(movies))
     }
   }
--- app/implicits/Utils.scala
+++ app/implicits/Utils.scala
@@ -1,11 +1,15 @@
 package implicits
 
-import scala.util.parsing.json.{JSONObject, JSONArray}
+import play.api.cache.CacheApi
+
+import scala.reflect.ClassTag
+import scala.util.parsing.json.{JSONArray, JSONObject}
 
 /**
   * Created by pabloperezgarcia on 27/8/16.
   */
 object Utils {
+
 
   implicit class anyUtils(any: Any) {
     def asStringMap: Map[String, Any] = any.asInstanceOf[Map[String, Any]]
@@ -17,6 +21,10 @@ object Utils {
 
   implicit class optionUtils(o: Option[Any]) {
     def asString: String = String.valueOf(o.get)
+  }
+
+  implicit class cacheUtils[T:ClassTag](cache:CacheApi){
+    def getVal(key:String): T = cache.get(key).get
   }
 
 }
